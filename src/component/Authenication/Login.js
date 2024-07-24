b6d7118811e83ad6630dadb9a41ad3a53da50d4d
Siddharth <siddharth@vasantsoftwaressolution.com>
2024-07-24 19:28:06 +0530

    make the app faster

--- src/component/Authenication/Login.js
+++ src/component/Authenication/Login.js
@@ -1,128 +1,150 @@
-import React, { useState } from 'react'
-import  { useNavigate } from 'react-router-dom'
-import { useToast } from '@chakra-ui/react'
-import { useAppContext } from '../../context/appContext';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useToast } from "@chakra-ui/react";
+import { useAppContext } from "../../context/appContext";
 
-import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, VStack } from '@chakra-ui/react'
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  VStack,
+} from "@chakra-ui/react";
 const Login = () => {
   const { loginUser } = useAppContext();
- const [show,setShow]= useState(false);
- const [email,setEmail]= useState('');
- const [password,setPassword]= useState('');
- const [loading, setLoading] = useState(false);
- const navigate = useNavigate();
- const toast = useToast()
- const postDetails = async() =>{
-  try {
-    
-     loginUser({email:"testuser@test.com", password:"testuser"})
-   
-   toast({
-     title: `Singed in Successfully.`,
-     status: "success",
-     position: 'top',
-     duration:5000,
-     isClosable: true,
-   })
-   setLoading(false);
-   navigate('/chats');
-    
- } catch (error) {
-   toast({
-     title: 'Error Occupied',
-     description: error.response.data.message,
-     status: "error",
-     duration: 3000,
-     position:"top",
-     isClosable: true,
-   })
-   setLoading(false);
- }
- }
- const submitHandler = async() =>{
-  if(!email || !password ){
-    toast({
-      title: `Please Fill All The Fields`,
-      status: "warning",
-      position: 'top',
-      duration:5000,
-      variant:"left-accent",
-      isClosable: true,
-    })
-   }
-   
-  try {
-     loginUser({email, password})
-    toast({
-      title: `Singed in Successfully.`,
-      status: "success",
-      position: 'top',
-      duration:5000,
-      isClosable: true,
-    })
-    setLoading(false);
-    navigate('/chats');
-     
-  } catch (error) {
-    toast({
-      title: 'Error Occupied',
-      description: error.response.data.message,
-      status: "error",
-      position:"top",
-      duration: 3000,
-      isClosable: true,
-    })
-    setLoading(false);
-  }
+  const [show, setShow] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const toast = useToast();
 
- }
+  useEffect(() => {
+    return () => {
+      if (localStorage.getItem("user-id")) {
+        navigate("/chats");
+      }
+    };
+  });
+  const postDetails = async () => {
+    try {
+      loginUser({ email: "testuser@test.com", password: "testuser" });
+
+      toast({
+        title: `Singed in Successfully.`,
+        status: "success",
+        position: "top",
+        duration: 5000,
+        isClosable: true,
+      });
+      setLoading(false);
+      navigate("/chats");
+    } catch (error) {
+      toast({
+        title: "Error Occupied",
+        description: error.response.data.message,
+        status: "error",
+        duration: 3000,
+        position: "top",
+        isClosable: true,
+      });
+      setLoading(false);
+    }
+  };
+  const submitHandler = async () => {
+    if (!email || !password) {
+      toast({
+        title: `Please Fill All The Fields`,
+        status: "warning",
+        position: "top",
+        duration: 5000,
+        variant: "left-accent",
+        isClosable: true,
+      });
+    }
+
+    try {
+      loginUser({ email, password });
+      toast({
+        title: `Singed in Successfully.`,
+        status: "success",
+        position: "top",
+        duration: 5000,
+        isClosable: true,
+      });
+      setLoading(false);
+      navigate("/chats");
+    } catch (error) {
+      toast({
+        title: "Error Occupied",
+        description: error.response.data.message,
+        status: "error",
+        position: "top",
+        duration: 3000,
+        isClosable: true,
+      });
+      setLoading(false);
+    }
+  };
   return (
     <VStack spacing={"5px"}>
-        
-        
-        <FormControl>
-          <FormLabel  className="text"  >Email</FormLabel>
-          <Input 
+      <FormControl>
+        <FormLabel className="text">Email</FormLabel>
+        <Input
           type={"email"}
           placeContent={"Enter Your Name"}
-          onChange={(e)=>{setEmail(e.target.value)}}
-          />
-        </FormControl>
-        <FormControl>
-          <FormLabel  className="text">Password</FormLabel>
-          <InputGroup>
-          <Input 
-          type={show? "text" :"password"}
-          placeContent={"Enter Your Name"}
-          onChange={(e)=>{setPassword(e.target.value)}}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+      </FormControl>
+      <FormControl>
+        <FormLabel className="text">Password</FormLabel>
+        <InputGroup>
+          <Input
+            type={show ? "text" : "password"}
+            placeContent={"Enter Your Name"}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
           />
           <InputRightElement width="4.5rem">
-            <Button h="1.75rem" size="sm" onClick={()=>{setShow(!show)}}>{show? "Hide" : "Show"}</Button>
+            <Button
+              h="1.75rem"
+              size="sm"
+              onClick={() => {
+                setShow(!show);
+              }}
+            >
+              {show ? "Hide" : "Show"}
+            </Button>
           </InputRightElement>
-          </InputGroup>
-        </FormControl>
-       
-        <Button
+        </InputGroup>
+      </FormControl>
+
+      <Button
         colorScheme="blue"
         width={"100%"}
         isLoading={loading}
-        style={{marginTop:15}}
+        style={{ marginTop: 15 }}
         onClick={submitHandler}
-        >
-          Sing in
-        </Button>
-        <Button
+      >
+        Sing in
+      </Button>
+      <Button
         variant={"solid"}
         colorScheme="red"
         width={"100%"}
         isLoading={loading}
-        style={{marginTop:15 , fontFamily:"-moz-initial"}}
+        style={{ marginTop: 15, fontFamily: "-moz-initial" }}
         onClick={postDetails}
-        >
-          EXPLORE THE APP
-        </Button>
+      >
+        EXPLORE THE APP
+      </Button>
     </VStack>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- src/context/appContext.js
+++ src/context/appContext.js
@@ -1,43 +1,38 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useReducer, useContext, useEffect } from 'react';
-import reducer from './reducer';
+import React, { useReducer, useContext, useEffect } from "react";
+import reducer from "./reducer";
 import {
   DISPLAY_ALERT,
   CLEAR_ALERT,
-
   REGISTER_USER_BEGIN,
   REGISTER_USER_ERROR,
   REGISTER_USER_SUCCESS,
-
   LOGIN_USER_BEGIN,
   LOGIN_USER_ERROR,
   LOGIN_USER_SUCCESS,
-
   LOGOUT_USER,
+} from "./actions";
+import axios from "axios";
+import { useState } from "react";
 
-
-} from './actions'
-import axios from 'axios';
-import { useState } from 'react';
-
-
+const user_email = localStorage.getItem("user-email");
+const user_name = localStorage.getItem("user-name");
+const user_pic = localStorage.getItem("user-pic");
+const user_token = localStorage.getItem("user-token");
+const user_id = localStorage.getItem("user-id");
 export const initialState = {
   showSidebar: false,
   isLoading: false,
   showAlert: false,
-  
-  alertText: '',
-  alertType: '',
-  user: '',
-  id: '',
-  name: '',
-  pic: '',
-  email: '',
+
+  alertText: "",
+  alertType: "",
+  user: user_email ||  "",
+  id: user_name ||  "",
+  name: user_pic ||  "",
+  pic: user_token ||  "",
+  email: user_id ||  "",
 };
-
-
-
-
 
 const AppContext = React.createContext();
 const AppProvider = ({ children }) => {
@@ -54,17 +49,17 @@ const AppProvider = ({ children }) => {
     width: undefined,
     height: undefined,
   });
- 
+
   const style = {
     backgroundImage: `url(${BGI})`,
     backgroundRepeat: "no-repeat",
     backgroundSize: "cover",
     backgroundPosition: "center",
-  }
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     // console.log(localStorage.getItem('userInfo'));
-  })
+  });
 
   useEffect(() => {
     function handleResize() {
@@ -81,9 +76,30 @@ const AppProvider = ({ children }) => {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
+  function setData(email, name, pic, token, id) {
+    localStorage.setItem("user-email", email);
+    localStorage.setItem("user-name", name);
+    localStorage.setItem("user-pic", pic);
+    localStorage.setItem("user-token", token);
+    localStorage.setItem("user-id", id);
+  }
+  function getData() {
+    localStorage.getItem("user-email");
+    localStorage.getItem("user-name");
+    localStorage.getItem("user-pic");
+    localStorage.getItem("user-token");
+    localStorage.getItem("user-id");
+  }
+  function logOut() {
+    localStorage.removeItem("user-email");
+    localStorage.removeItem("user-name");
+    localStorage.removeItem("user-pic");
+    localStorage.removeItem("user-token");
+    localStorage.removeItem("user-id");
+  }
 
   const sp = axios.create({
-    baseURL: 'https://chatserver-962m.onrender.com',
+    baseURL: "https://chatserver-962m.onrender.com",
     // baseURL: 'http://localhost:5000',
     headers: {
       Authorization: `Bearer ${state.user}`,
@@ -92,7 +108,7 @@ const AppProvider = ({ children }) => {
   // response interceptor
   sp.interceptors.request.use(
     (config) => {
-      config.headers['Authorization'] = `Bearer ${state.user}`;
+      config.headers["Authorization"] = `Bearer ${state.user}`;
       return config;
     },
     (error) => {
@@ -112,7 +128,6 @@ const AppProvider = ({ children }) => {
     }
   );
 
-
   const displayAlert = () => {
     dispatch({
       type: DISPLAY_ALERT,
@@ -128,20 +143,22 @@ const AppProvider = ({ children }) => {
     }, 3000);
   };
 
-
   const registerUser = async (currentUser) => {
     dispatch({ type: REGISTER_USER_BEGIN });
     try {
-      const response = await sp.post('/users', currentUser);
+      const response = await sp.post("/users", currentUser);
       const { email, name, pic, token, _id } = response.data;
+      setData(email, name, pic, token, _id);
       dispatch({
         type: REGISTER_USER_SUCCESS,
         payload: {
-          email, name, pic, token, _id
-
+          email,
+          name,
+          pic,
+          token,
+          _id,
         },
       });
-
     } catch (error) {
       dispatch({
         type: REGISTER_USER_ERROR,
@@ -157,18 +174,22 @@ const AppProvider = ({ children }) => {
     dispatch({ type: LOGIN_USER_BEGIN });
     console.log("start2");
     try {
-      const response = await sp.post('/users/login', currentUser);
+      const response = await sp.post("/users/login", currentUser);
       console.log("start3");
 
       const { email, name, pic, token, _id } = response.data;
+      setData(email, name, pic, token, _id);
       dispatch({
         type: LOGIN_USER_SUCCESS,
         payload: {
-          email, name, pic, token, _id
-
-        }
+          email,
+          name,
+          pic,
+          token,
+          _id,
+        },
       });
-        
+
       console.log("start4");
     } catch (error) {
       dispatch({
@@ -180,11 +201,10 @@ const AppProvider = ({ children }) => {
     clearAlert();
   };
 
-
   const logoutUser = () => {
-    dispatch({ type: LOGOUT_USER })
-  }
-
+    dispatch({ type: LOGOUT_USER });
+    logOut();
+  };
 
   return (
     <AppContext.Provider
@@ -202,10 +222,9 @@ const AppProvider = ({ children }) => {
         setChats,
         sp,
         style,
-        BGI,setBGI,
+        BGI,
+        setBGI,
         windowSize,
-
-
       }}
     >
       {children}
@@ -216,7 +235,5 @@ const AppProvider = ({ children }) => {
 export const useAppContext = () => {
   return useContext(AppContext);
 };
-
-
 
 export default AppProvider;
